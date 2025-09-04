8cab97be105be29f95e57e04b98521528bd1e801
Priyanshu Harshbodhi <priyanshuqpwp@gmail.com>
2025-09-04 15:20:00 +0530

    docs: translate Chinese comments and clean up prompt files

--- tello_wrapper.py
+++ tello_wrapper.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2023/12/23  22:24
-# @Author  : mariswang@rflysim
-# @File    : tello_wrapper.py
-# @Software: PyCharm
-# @Describe: 
-# -*- encoding:utf-8 -*-
-
 
 import sys
 import threading
@@ -32,13 +24,13 @@ class TelloWrapper:
         self.client.connect()
 
 
-        #启动视频流
-        self.client.streamon()  # 开启视频传输
+        # Start video stream
+        self.client.streamon()  # Enable video transmission
         t = threading.Thread(target=self.get_stream)
         t.setDaemon(True)
         t.start()
 
-        self.head_img = None #tello 前置摄像头
+        self.head_img = None # Tello front camera
 
 
 
@@ -56,72 +48,72 @@ class TelloWrapper:
 
     def turn_left(self,degree=10):
         """
-        左转degree度
+        Turn left by degree degrees
         :return:
         """
-        degree = 10*degree #tello base is 0.1度
+        degree = 10*degree # tello base is 0.1 degrees
         self.client.rotate_counter_clockwise(degree)
 
 
     def turn_right(self,degree=10):
         """
-        右转degree度
+        Turn right by degree degrees
         :return:
         """
-        degree = 10*degree #tello base is 0.1度
+        degree = 10*degree # tello base is 0.1 degrees
         self.client.rotate_clockwise(degree)
 
 
     def forward(self, distance):
         """
-        向前移动, 太少了不动
-        distance: 距离，米
+        Move forward, too small movements won't work
+        distance: distance in meters
         :return:
         """
         distance = distance*100 # tello base is 1cm
-        self.client.move_forward(distance) #向前移动50cm
+        self.client.move_forward(distance) # Move forward
 
 
     def back(self, distance):
         """
-        向后移动, 太少了不动
-        distance: 距离，米
+        Move backward, too small movements won't work
+        distance: distance in meters
         :return:
         """
         distance = distance*100 # tello base is 1cm
-        self.client.move_back(distance) #向前移动50cm
+        self.client.move_back(distance) # Move backward
 
 
     def up(self, distance):
         """
-        向上移动, 太少了不动
-        distance: 距离，米
+        Move up, too small movements won't work
+        distance: distance in meters
         :return:
         """
         distance = distance*100 # tello base is 1cm
-        self.client.move_up(distance) #向前移动50cm
+        self.client.move_up(distance) # Move up
 
 
     def down(self, distance):
         """
-        向下移动, 太少了不动
-        distance: 距离，米
+        Move down, too small movements won't work
+        distance: distance in meters
         :return:
         """
         distance = distance*100 # tello base is 1cm
-        self.client.move_down(distance) #向前移动50cm
+        self.client.move_down(distance) # Move down
         
         
     def get_image(self):
         """
-        获得前置摄像头渲染图像
+        Get front camera rendered image
         :return:
         """
         return self.head_img
 
     def get_drone_state(self):
         """
-        获得无人机状态,
+        Get drone state
         :return:{'pitch': int, 'roll': int, 'yaw': int}
         """
         return self.client.query_attitude()
@@ -134,20 +126,20 @@ class TelloWrapper:
     
     def ob_objects(self,obj_name_list):
         """
-        注意需要先执行get_image，
-        在图像 img 上运行对象检测模型，获得目标列表 [ <对象名称、距离、角度（以度为单位）>,...]
-        :return:对象名称列表、对象信息列表、bbox图
+        Note: need to execute get_image first,
+        Run object detection model on image img, get target list [<object_name, distance, angle_in_degrees>,...]
+        :return: object name list, object info list, bbox image
         """
         pass
 
 
     def ob_objects_llm(self,obj_name_list):
         """
-        注意需要先执行get_image，为llm提供观测结果
-        在图像 img 上运行对象检测模型，获得目标列表 [ <对象名称、距离、角度（以度为单位）>,...] , 给到llm用于推理
-        :return:[ <对象名称、距离、角度（以度为单位）>,...] 如[(门，0.53，22)，(椅子，4.84，-21)]
+        Note: need to execute get_image first, provide observation results for llm
+        Run object detection model on image img, get target list [<object_name, distance, angle_in_degrees>,...] for llm reasoning
+        :return: [<object_name, distance, angle_in_degrees>,...] e.g. [(door, 0.53, 22), (chair, 4.84, -21)]
         """
-        #获得识别结果
+        # Get recognition results
         ob_list, final_obj_list, annotated_frame = self.ob_objects(obj_name_list)
 
         final_result = []
